5dd6cce0cb9ee970be4c8265bf2be125958ba189
Fleur41 <Muriithikings456@gmail.com>
2024-01-15 12:01:31 +0300

    update_score

--- Pong.py
+++ Pong.py
@@ -2,3 +2,17 @@
 Pong Arcade Game
 -------------------------------------------------------------
 '''
+
+import turtle
+
+def update_score(l_score, r_score, player, score_board):
+    if player == 'l':
+        l_score += 1
+    else:
+        r_score += 1
+
+    score_board.clear()
+    score_board.write('Left Player: {} -- Right Player: {}'.format(
+        l_score, r_score), align='center',
+        font=('Arial', 24, 'normal'))
+    return l_score, r_score, score_board
